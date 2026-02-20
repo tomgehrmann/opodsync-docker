b6af414d98280865a91ed440a2fa41a06d5adc7d
Tom Gehrmann <63705789+tomgehrmann@users.noreply.github.com>
2026-02-20 14:19:44 +0100

    feat: production web server

---
+++ config.dist.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace OPodSync;
+
+/**
+ * ERRORS_SHOW
+ * Show PHP errors details to users?
+ * If set to TRUE, full error messages and source code will be displayed to visitors.
+ * If set to FALSE, just a generic "an error happened" message will be displayed.
+ *
+ * It is recommended to set this to FALSE in production.
+ * Default: TRUE
+ *
+ * @var bool
+ */
+const ERRORS_SHOW = false;
